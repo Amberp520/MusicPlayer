1d72b579f27e5ccabbadad597a4188116efa3681
HP <emenikep520@gmail.com>
2024-03-11 23:09:45 -0700

    Add project files.

---
+++ Music.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace MusicPlayer
+{
+    internal class Music
+    {
+        static void Main(string[] args)
+        {
+            PlayMusic("chiling39-20915");
+            Console.WriteLine("Playing music");
+            Console.ReadLine();
+
+        }
+
+        public static void PlayMusic(string filepath)
+        {
+            SoundPlayer musicPlayer = new SoundPlayer();
+            musicPlayer.SoundLocation = filepath;
+            musicPlayer.Play();
+        }
+
+        private class SoundPlayer
+        {
+            public string? SoundLocation { get; internal set; }
+
+            internal void Play()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicPlayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Music** Music Begins to Play");
+        }
+    }
+}
